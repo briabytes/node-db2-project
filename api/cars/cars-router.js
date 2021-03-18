57459d9900a076933b9e276f00a338cca3995b14
bria barry <briabarry@iMac.lan>
2021-03-17 20:56:16 -0700

    MVP

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,36 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Car = require('./cars-model');
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+} = require('./cars-middleware');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars);
+    }catch(e) {
+        next(e);
+    }
+});
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        res.json(req.car);
+    }catch(e) {
+        next(e);
+    }
+});
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+    try {
+        const newCar = await Car.create(req.body);
+        res.status(201).json(newCar);
+    }catch(e) {
+        next(e);
+    }
+});
+
+module.exports = router;
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('cars', tbl => {
+            tbl.increments('id')
+            tbl.text('vin').unique().notNullable()
+            tbl.text('make').notNullable()
+            tbl.text('model').notNullable()
+            tbl.decimal('mileage').notNullable()
+            tbl.text('title').notNullable()
+            tbl.text('transmission').notNullable()
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('cars')
+}
